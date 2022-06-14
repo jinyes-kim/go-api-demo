0ca505b063f260cda74ea2299252841a9296aa84
Jinung kim <jinung_kim@deali.net>
2022-06-14 12:16:58 +0900

    Add logger middleware

--- api/main.go
+++ api/main.go
@@ -1,9 +1,15 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
 
 func main() {
 	app := fiber.New()
+	app.Use(logger.New(logger.Config{
+		Format: "[${ip}]:${port} ${status} - ${method} ${path}\n",
+	}))
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World! from docker go fiber")
